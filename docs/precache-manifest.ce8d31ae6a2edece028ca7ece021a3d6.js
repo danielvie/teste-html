5a6bda98d9789897e3bbebbb95aaee8f44f41429
daniel <danielvie@gmail.com>
2020-09-27 01:52:10 -0300

    bla

---
+++ docs/precache-manifest.ce8d31ae6a2edece028ca7ece021a3d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df6e5b949c084ca1df9",
+    "url": "/teste-html/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/teste-html/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fb9394a6c6fdf9b3c7c924185272d340",
+    "url": "/teste-html/index.html"
+  },
+  {
+    "revision": "3df6e5b949c084ca1df9",
+    "url": "/teste-html/js/app.d76aed11.js"
+  },
+  {
+    "revision": "3da8b983d60c4988ecbe",
+    "url": "/teste-html/js/chunk-vendors.58e26f54.js"
+  },
+  {
+    "revision": "c18df4d66a05bda491875aae4196485a",
+    "url": "/teste-html/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/teste-html/robots.txt"
+  }
+]);
